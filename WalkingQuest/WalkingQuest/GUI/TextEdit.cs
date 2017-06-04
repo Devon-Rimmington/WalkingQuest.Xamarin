54bbf846f0f1bb60cfaa5d698d568968f3b0c8c1
noved210 <devonrimmington@gmail.com>
2017-06-04 19:42:53 -0300

    rebase commit

---
+++ WalkingQuest/WalkingQuest/GUI/TextEdit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Urho;
+using Urho.Gui;
+
+namespace WalkingQuest.GUI
+{
+    class TextEdit: LineEdit
+    {
+
+        private const String defaultFont = "Fonts/Font.ttf";
+        private const int defaultFontSize = 24;
+        private Color defaultColor = new Color(0f, 0f, 0f); // black
+
+        public TextEdit(Context c, Application application) : base(c)
+        {
+            TextElement.SetColor(defaultColor);
+            TextElement.SetFont(application.ResourceCache.GetFont(defaultFont), size: defaultFontSize);
+            TextElement.SetAlignment(HorizontalAlignment.Left, VerticalAlignment.Center);
+        }
+
+
+    }
+}
---
+++ WalkingQuest/WalkingQuest/UIHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Urho;
+using Urho.Gui;
+using Urho.Resources;
+
+namespace WalkingQuest
+{
+    class UIHelper
+    {
+
+        private static Color _black = new Color(0f, 0f, 0f);
+        private static int defaultFontSize = 24;
+        private static String defaultFont = "Fonts/Font.ttf";
+
+        public UIHelper() { }
+
+        // SETS THE TEXT WITHIN THE LINEEDIT UI ELEMENTS ###################################################################
+
+        public static Text setLineEditText(Text text, Application application)
+        {
+
+            if (text != null && application != null)
+            {
+                text.SetColor(_black);
+                text.SetFont(application.ResourceCache.GetFont(defaultFont), size: defaultFontSize);
+
+                text.SetAlignment(HorizontalAlignment.Center, VerticalAlignment.Center);
+
+                return text;
+            }
+
+            return null;
+
+        }
+
+        public static Text setLineEditText(Text text, Application application, String font, int fontSize, Color color)
+        {
+
+            if (text != null && application != null)
+            {
+                text.SetColor(color);
+                text.SetFont(application.ResourceCache.GetFont("Fonts/"+font), size: fontSize);
+                text.SetAlignment(HorizontalAlignment.Center, VerticalAlignment.Center);
+                return text;
+            }
+
+            return null;
+
+        }
+
+        // #################################################################################################################
+
+
+    }
+}
